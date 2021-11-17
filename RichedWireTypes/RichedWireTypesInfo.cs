02e3cf69b00435e2949f0993c7a721788072e376
秋水 <1123993881@qq.com>
2021-11-17 14:26:51 +0800

    Add project files.

---
+++ RichedWireTypes/RichedWireTypesInfo.cs
@@ -0,0 +1,72 @@
+﻿using Grasshopper;
+using Grasshopper.GUI;
+using Grasshopper.GUI.Canvas;
+using Grasshopper.Kernel;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace RichedWireTypes
+{
+    public class RichedWireTypesInfo : GH_AssemblyInfo
+    {
+        public override string Name => "RichedWireTypes";
+
+        //Return a 24x24 pixel bitmap to represent this GHA library.
+        public override Bitmap Icon => null;
+
+        //Return a short string describing the purpose of this GHA library.
+        public override string Description => "Provide a set of wire types such as Polyline.";
+
+        public override Guid Id => new Guid("2850F61C-B64C-4466-8B9C-D89754AB6ECD");
+
+        //Return a string identifying you or your company.
+        public override string AuthorName => "秋水";
+
+        //Return a string representing your preferred contact details.
+        public override string AuthorContact => "1123993881@qq.com";
+
+        public override string Version => "0.9.0";
+    }
+
+    public class SuperHelperAssemblyPriority : GH_AssemblyPriority
+    {
+        public override GH_LoadingInstruction PriorityLoad()
+        {
+            //MessageBox.Show("Hello");
+            Grasshopper.Instances.CanvasCreated += Instances_CanvasCreated;
+            return GH_LoadingInstruction.Proceed;
+        }
+
+        private void Instances_CanvasCreated(GH_Canvas canvas)
+        {
+            Grasshopper.Instances.CanvasCreated -= Instances_CanvasCreated;
+
+            GH_DocumentEditor editor = Grasshopper.Instances.DocumentEditor;
+            if (editor == null)
+            {
+                Grasshopper.Instances.ActiveCanvas.DocumentChanged += ActiveCanvas_DocumentChanged;
+                return;
+            }
+            DoingSomethingFirst(editor);
+        }
+
+        private void ActiveCanvas_DocumentChanged(GH_Canvas sender, GH_CanvasDocumentChangedEventArgs e)
+        {
+            Grasshopper.Instances.ActiveCanvas.DocumentChanged -= ActiveCanvas_DocumentChanged;
+
+            GH_DocumentEditor editor = Grasshopper.Instances.DocumentEditor;
+            if (editor == null)
+            {
+                MessageBox.Show("SuperHelper can't find the menu!");
+                return;
+            }
+            DoingSomethingFirst(editor);
+        }
+
+        private void DoingSomethingFirst(GH_DocumentEditor editor)
+        {
+            WireDrawReplacer.Init();
+        }
+    }
+}
---
+++ RichedWireTypes/WireDrawReplacer.cs
@@ -0,0 +1,66 @@
+﻿using Grasshopper.GUI.Canvas;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RichedWireTypes
+{
+    internal abstract class WireDrawReplacer : GH_Painter
+    {
+        protected WireDrawReplacer(GH_Canvas owner) : base(owner)
+        {
+        }
+
+        public static GraphicsPath NewConnectionPath(PointF pointA, PointF pointB, GH_WireDirection directionA, GH_WireDirection directionB)
+        {
+            //BezierF bezierF = ((directionA != 0) ? ConnectionPathBezier(pointA, pointB).Reverse() : ConnectionPathBezier(pointB, pointA));
+            GraphicsPath graphicsPath = new GraphicsPath();
+            graphicsPath.AddLine(pointA, pointB);
+            return graphicsPath;
+        }
+
+        public static bool Init()
+        {
+            return ExchangeMethod(
+                typeof(GH_Painter).GetRuntimeMethods().Where(m => m.Name.Contains("ConnectionPath")).First(),
+                typeof(WireDrawReplacer).GetRuntimeMethods().Where(m => m.Name.Contains("NewConnectionPath")).First()
+                );
+        }
+
+        private static bool ExchangeMethod(MethodInfo targetMethod, MethodInfo injectMethod)
+        {
+            if (targetMethod == null || injectMethod == null)
+            {
+                return false;
+            }
+            RuntimeHelpers.PrepareMethod(targetMethod.MethodHandle);
+            RuntimeHelpers.PrepareMethod(injectMethod.MethodHandle);
+            unsafe
+            {
+                if (IntPtr.Size == 4)
+                {
+                    int* tar = (int*)targetMethod.MethodHandle.Value.ToPointer() + 2;
+                    int* inj = (int*)injectMethod.MethodHandle.Value.ToPointer() + 2;
+                    var relay = *tar;
+                    *tar = *inj;
+                    *inj = relay;
+                }
+                else
+                {
+                    long* tar = (long*)targetMethod.MethodHandle.Value.ToPointer() + 1;
+                    long* inj = (long*)injectMethod.MethodHandle.Value.ToPointer() + 1;
+                    var relay = *tar;
+                    *tar = *inj;
+                    *inj = relay;
+                }
+            }
+            return true;
+        }
+    }
+}
