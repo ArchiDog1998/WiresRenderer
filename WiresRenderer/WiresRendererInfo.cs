5a6eb77ebfd9dd120869dbd97eac3929c4cebd2a
秋水 <1123993881@qq.com>
2023-02-07 15:01:09 +0800

    1.3.9 release.

--- WiresRenderer/WiresRendererInfo.cs
+++ WiresRenderer/WiresRendererInfo.cs
@@ -26,7 +26,7 @@ namespace WiresRenderer
         //Return a string representing your preferred contact details.
         public override string AuthorContact => "1123993881@qq.com";
 
-        public override string Version => "1.3.8";
+        public override string Version => "1.3.9";
     }
 
     public class SuperHelperAssemblyPriority : GH_AssemblyPriority
